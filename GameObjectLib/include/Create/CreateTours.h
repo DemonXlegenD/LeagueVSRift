b4f72123e5533e56fd0d7677c5e26ec0c3de564a
p0ison <quentinleforestier0@gmail.com>
2023-11-07 19:56:07 +0100

    c'est tout bon

--- GameObjectLib/include/Create/CreateTours.h
+++ GameObjectLib/include/Create/CreateTours.h
@@ -2,6 +2,7 @@
 #include <vector>
 #include "GameObject.h"
 #include "SceneManager.h"
+#include "Components/Ressource.h"
 
 class CreateTours
 {
--- GameObjectLib/src/Create/CreateTours.cpp
+++ GameObjectLib/src/Create/CreateTours.cpp
@@ -3,32 +3,32 @@
 
 void CreateTours::CreateJiin(float _poisitionX, float _positionY, sf::Texture _texture) 
 {
-	if (SceneManager::GetActiveGameScene()->GetGameObject("Jiin")->GetPrixGold() <= SceneManager::GetActiveGameScene()->GetGameObject("Gold")->GetRessource() && 
-		SceneManager::GetActiveGameScene()->GetGameObject("Jiin")->GetPrixMana() <= SceneManager::GetActiveGameScene()->GetGameObject("Mana")->GetRessource())
+	if (SceneManager::GetActiveGameScene()->GetGameObject("Jiin")->GetComponent<Ressource>()->GetRessource() <= SceneManager::GetActiveGameScene()->GetGameObject("Gold")->GetComponent<Ressource>()->GetRessource() &&
+		SceneManager::GetActiveGameScene()->GetGameObject("Jiin")->GetComponent<Ressource>()->GetRessource() <= SceneManager::GetActiveGameScene()->GetGameObject("Mana")->GetComponent<Ressource>()->GetRessource())
 	{
 		tower.push_back(SceneManager::GetActiveGameScene()->CreateBatimantGameObject("Jiin", _poisitionX, _positionY, _texture, 4.f, 4.f, 400.f, 30.f));
 	}
 }
 void CreateTours::CreateLulu(float _poisitionX, float _positionY, sf::Texture _texture)
 {
-	if (SceneManager::GetActiveGameScene()->GetGameObject("Lulu")->GetPrixGold() <= SceneManager::GetActiveGameScene()->GetGameObject("Gold")->GetRessource() &&
-		SceneManager::GetActiveGameScene()->GetGameObject("Lulu")->GetPrixGold() <= SceneManager::GetActiveGameScene()->GetGameObject("Mana")->GetRessource())
+	if (SceneManager::GetActiveGameScene()->GetGameObject("Lulu")->GetComponent<Ressource>()->GetRessource() <= SceneManager::GetActiveGameScene()->GetGameObject("Gold")->GetComponent<Ressource>()->GetRessource() &&
+		SceneManager::GetActiveGameScene()->GetGameObject("Lulu")->GetComponent<Ressource>()->GetRessource() <= SceneManager::GetActiveGameScene()->GetGameObject("Mana")->GetComponent<Ressource>()->GetRessource())
 	{
 		tower.push_back(SceneManager::GetActiveGameScene()->CreateBatimantGameObject("Lulu", _poisitionX, _positionY, _texture, 4.f, 4.f, 400.f, 30.f));
 	}
 }
 void CreateTours::CreateMalphite(float _poisitionX, float _positionY, sf::Texture _texture)
 {
-	if (SceneManager::GetActiveGameScene()->GetGameObject("Malphite")->GetPrixGold() <= SceneManager::GetActiveGameScene()->GetGameObject("Gold")->GetRessource() &&
-		SceneManager::GetActiveGameScene()->GetGameObject("Malphite")->GetPrixMana() <= SceneManager::GetActiveGameScene()->GetGameObject("Mana")->GetRessource())
+	if (SceneManager::GetActiveGameScene()->GetGameObject("Malphite")->GetComponent<Ressource>()->GetRessource() <= SceneManager::GetActiveGameScene()->GetGameObject("Gold")->GetComponent<Ressource>()->GetRessource() &&
+		SceneManager::GetActiveGameScene()->GetGameObject("Malphite")->GetComponent<Ressource>()->GetRessource() <= SceneManager::GetActiveGameScene()->GetGameObject("Mana")->GetComponent<Ressource>()->GetRessource())
 	{
 		tower.push_back(SceneManager::GetActiveGameScene()->CreateBatimantGameObject("Malphite", _poisitionX, _positionY, _texture, 4.f, 4.f, 400.f, 30.f));
 	}
 }
 void CreateTours::CreateXinZhao(float _poisitionX, float _positionY, sf::Texture _texture)
 {
-	if (SceneManager::GetActiveGameScene()->GetGameObject("XinZhao")->GetPrixGold() <= SceneManager::GetActiveGameScene()->GetGameObject("Gold")->GetRessource() &&
-		SceneManager::GetActiveGameScene()->GetGameObject("XinZhao")->GetPrixMana() <= SceneManager::GetActiveGameScene()->GetGameObject("Mana")->GetRessource())
+	if (SceneManager::GetActiveGameScene()->GetGameObject("XinZhao")->GetComponent<Ressource>()->GetRessource() <= SceneManager::GetActiveGameScene()->GetGameObject("Gold")->GetComponent<Ressource>()->GetRessource() &&
+		SceneManager::GetActiveGameScene()->GetGameObject("XinZhao")->GetComponent<Ressource>()->GetRessource() <= SceneManager::GetActiveGameScene()->GetGameObject("Mana")->GetComponent<Ressource>()->GetRessource())
 	{
 		tower.push_back(SceneManager::GetActiveGameScene()->CreateBatimantGameObject("XinZhao", _poisitionX, _positionY, _texture, 4.f, 4.f, 400.f, 30.f));
 	}
@@ -36,8 +36,8 @@ void CreateTours::CreateXinZhao(float _poisitionX, float _positionY, sf::Texture
 
 void CreateTours::CreateBat2(float _poisitionX, float _positionY, sf::Texture _texture)
 {
-	if (SceneManager::GetActiveGameScene()->GetGameObject("Bat2")->GetPrixGold() <= SceneManager::GetActiveGameScene()->GetGameObject("Gold")->GetRessource() &&
-		SceneManager::GetActiveGameScene()->GetGameObject("Bat2")->GetPrixGold() <= SceneManager::GetActiveGameScene()->GetGameObject("Mana")->GetRessource())
+	if (SceneManager::GetActiveGameScene()->GetGameObject("Bat2")->GetComponent<Ressource>()->GetRessource() <= SceneManager::GetActiveGameScene()->GetGameObject("Gold")->GetComponent<Ressource>()->GetRessource() &&
+		SceneManager::GetActiveGameScene()->GetGameObject("Bat2")->GetComponent<Ressource>()->GetRessource() <= SceneManager::GetActiveGameScene()->GetGameObject("Mana")->GetComponent<Ressource>()->GetRessource())
 	{
 		tower.push_back(SceneManager::GetActiveGameScene()->CreateBatimantGameObject("Bat2", _poisitionX, _positionY, _texture, 4.f, 4.f, 400.f, 0.f));
 	}
@@ -45,8 +45,8 @@ void CreateTours::CreateBat2(float _poisitionX, float _positionY, sf::Texture _t
 
 void CreateTours::CreateBat3(float _poisitionX, float _positionY, sf::Texture _texture)
 {
-	if (SceneManager::GetActiveGameScene()->GetGameObject("Bat3")->GetPrixGold() <= SceneManager::GetActiveGameScene()->GetGameObject("Gold")->GetRessource() &&
-		SceneManager::GetActiveGameScene()->GetGameObject("Bat3")->GetPrixGold() <= SceneManager::GetActiveGameScene()->GetGameObject("Mana")->GetRessource())
+	if (SceneManager::GetActiveGameScene()->GetGameObject("Bat3")->GetComponent<Ressource>()->GetRessource() <= SceneManager::GetActiveGameScene()->GetGameObject("Gold")->GetComponent<Ressource>()->GetRessource() &&
+		SceneManager::GetActiveGameScene()->GetGameObject("Bat3")->GetComponent<Ressource>()->GetRessource() <= SceneManager::GetActiveGameScene()->GetGameObject("Mana")->GetComponent<Ressource>()->GetRessource())
 	{
 		tower.push_back(SceneManager::GetActiveGameScene()->CreateBatimantGameObject("Bat3", _poisitionX, _positionY, _texture, 4.f, 4.f, 400.f, 5.f));
 	}
