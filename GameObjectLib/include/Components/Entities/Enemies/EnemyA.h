dbc990e77e0001875a4f5af3f5bec575164d4dd1
KakaTheRat <kais.difallah@gmail.com>
2023-11-08 14:42:12 +0100

    Ennemies values ok

---
+++ GameObjectLib/include/Components/Entities/Enemies/EnemyA.h
@@ -0,0 +1,10 @@
+#include "Components/Entities/EnemyAbstract.h"
+#include "Components/SpriteRenderer.h"
+
+class EnemyA : public EnemyAbstract {
+public:
+	EnemyA();
+	~EnemyA();
+private:
+	Sprite sprite;
+};
---
+++ GameObjectLib/src/Components/Entities/Enemies/EnemyB.cpp
@@ -0,0 +1,4 @@
+#include "Components/Entities/Enemies/EnemyB.h"
+#include "Components/SpriteRenderer.h"
+
+EnemyB::EnemyB(int _lane) : EnemyAbstract(50, 3, _lane, 0.5f, 0.5f) {};
